cd1301a483c39a5afd8f9f8ab78442205f8a1e4d
inydv <i_nydv@hotmail.com>
2023-12-12 19:49:38 +0530

    change image src

--- dist/assets/Dashboard.Page-57b17608.js
+++
@@ -1,1 +0,0 @@
-import{r,a}from"./index-90f43bfb.js";function s(){return r.useEffect(()=>{window.scrollTo(0,0)},[]),a("div",{children:"Dashboard.Page"})}export{s as default};
---
+++ dist/assets/Dashboard.Page-994e911e.js
@@ -0,0 +1,1 @@
+import{r,a}from"./index-addfd89c.js";function s(){return r.useEffect(()=>{window.scrollTo(0,0)},[]),a("div",{children:"Dashboard.Page"})}export{s as default};
