450c191fee89d61c0e00ce60d63cb6bc28607c5b
inydv <i_nydv@hotmail.com>
2024-01-14 22:18:06 +0530

    change dist

--- dist/assets/Loader.Layout-6c7ba680.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-8d768b91.js";function n(){return e("section",{className:"h-screen w-screen grid place-content-center bg-black fixed top-0 left-0 z-50",children:e("div",{className:"w-28 h-28 border-b-4 border-b-red-600 rounded-full animate-spin"})})}export{n as default};
---
+++ dist/assets/Protected.Routes-55c7338b.js
@@ -0,0 +1,1 @@
+import{a as s,u as a,r,j as u,O as o}from"./index-11d7ad4e.js";function c(){const{user:t}=s(),e=a();return r.useEffect(()=>{t||e("/")},[e,t]),u(o,{})}export{c as default};
