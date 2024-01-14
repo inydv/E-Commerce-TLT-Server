4f7797c71c10141dd1f6168f421be4cf33028f3c
inydv <i_nydv@hotmail.com>
2024-01-14 15:11:32 +0530

    add dist

---
+++ dist/assets/Authentication.Routes-076e6fdd.js
@@ -0,0 +1,1 @@
+import{a as s,u as a,r,j as u,O as n}from"./index-27f57fcd.js";function i(){const{user:t}=s(),e=a();return r.useEffect(()=>{t&&e("/")},[e,t]),u(n,{})}export{i as default};
---
+++ dist/assets/Date.Pipe-a7fb7a4a.js
@@ -0,0 +1,1 @@
+function i(c){return c==null?void 0:c.slice(0,10)}export{i as D};
