1f26f6a30b7989b11a81cd3027200127d7fa6b14
Lokesh Yadav <i_nydv@hotmail.com>
2025-01-14 11:08:51 +0530

    Delete dist directory

--- dist/assets/Date.Pipe-a7fb7a4a.js
+++
@@ -1,1 +0,0 @@
-function i(c){return c==null?void 0:c.slice(0,10)}export{i as D};
--- dist/assets/Protected.Routes-42b1f2ce.js
+++
@@ -1,1 +0,0 @@
-import{a as s,u as a,r,j as u,O as o}from"./index-59e93b5a.js";function c(){const{user:t}=s(),e=a();return r.useEffect(()=>{t||e("/")},[e,t]),u(o,{})}export{c as default};
