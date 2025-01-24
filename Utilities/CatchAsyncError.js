d6362446bc04c3c09489189764cd48153aadee06
Lokesh Yadav <i_nydv@hotmail.com>
2025-01-24 22:24:48 +0530

    Update CatchAsyncError.js

--- Utilities/CatchAsyncError.js
+++ Utilities/CatchAsyncError.js
@@ -2,3 +2,28 @@
 module.exports = (theFunc) => (req, res, next) => {
   Promise.resolve(theFunc(req, res, next)).catch(next);
 };
+
+
+
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//   };
+// };
+
+// export { asyncHandler };
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
