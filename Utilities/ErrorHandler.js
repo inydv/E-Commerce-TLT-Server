237b19cc066808c794b96567458fae91b8c8727b
Lokesh Yadav <i_nydv@hotmail.com>
2025-01-24 22:24:25 +0530

    Update ErrorHandler.js

--- Utilities/ErrorHandler.js
+++ Utilities/ErrorHandler.js
@@ -9,3 +9,29 @@ class ErrorHandler extends Error {
 
 // export
 module.exports = ErrorHandler;
+
+
+
+// class ApiError extends Error {
+//   constructor(
+//     statusCode,
+//     message = "Something went wrong",
+//     errors = [],
+//     stack = ""
+//   ) {
+//     super(message);
+//     this.statusCode = statusCode;
+//     this.data = null;
+//     this.message = message;
+//     this.success = false;
+//     this.errors = errors;
+
+//     if (stack) {
+//       this.stack = stack;
+//     } else {
+//       Error.captureStackTrace(this, this.constructor);
+//     }
+//   }
+// }
+
+// export { ApiError };
